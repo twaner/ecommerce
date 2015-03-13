c89c0d78ddb1c4f0a731abca86b234613e09bfa0
t.waner <t.waner@gmail.com>
2015-03-13 16:32:55 -0400

    added order model

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'products',
     'carts',
+    'orders',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ ecommerce/orders/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from carts.models import Cart
+
+# Create your models here.
+
+STATUS_CHOICES = (
+    ("Started", "Started"),
+    ("Abandoned", "Abandoned"),
+    ("Finished", "Finished"),
+)
+
+
+class Order(models.Model):
+    # TODO user, adddress(?),
+    order_id = models.CharField(max_length=120, default="ABC", unique=True)
+    cart = models.ForeignKey(Cart)
+    status = models.CharField(max_length=120, choices=STATUS_CHOICES, default='Started')
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.order_id
