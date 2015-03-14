a9b65ebc7121baf6fe08d79047eba3aac3fb0864
t.waner <t.waner@gmail.com>
2015-03-14 17:44:23 -0400

    registration form & unique email. lecture 83-84

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth import login, logout, authenticate
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 # Create your views here.
 
@@ -26,3 +26,22 @@ def login_view(request):
 
     return render(request, "form.html", context)
 
+
+def registration_view(request):
+    form = RegistrationForm(request.POST or None)
+
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        # first_name = form.cleaned_data['first_name']
+        # last_name = form.cleaned_data['last_name']
+        new_user.save() #runs form's save method
+        # username = form.cleaned_data['username']
+        # password = form.cleaned_data['password']
+        # user = authenticate(username=username, password=password)
+        # login(request, user)
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "form.html", context)
