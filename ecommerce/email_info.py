bd8e69cce1a9c9cc62b2644ada9f5115cdf82ed7
Taiowa Waner <t.waner@gmail.com>
2015-03-16 17:26:13 -0400

    email settings file

--- ecommerce/email_info.py
+++ ecommerce/email_info.py
@@ -1,1 +1,7 @@
 # information for sending
+
+EMAIL_HOST = 'smpt.gmail.com' #smpt.sendgrid.com
+EMAIL_HOST_USER = 'taiowawaner'
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_USE_TLS = True # contact provider
+EMAIL_PORT = 587 # contact provider
